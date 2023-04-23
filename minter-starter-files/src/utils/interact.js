af4c79d29607483192f6e11d91a49da11f06dc4a
Adriano <62570164+AdrianGeorgeM@users.noreply.github.com>
2023-04-23 18:28:40 +0100

    Added input error handling for mintNFT function

--- minter-starter-files/src/utils/interact.js
+++ minter-starter-files/src/utils/interact.js
@@ -1,3 +1,21 @@
+require('dotenv').config();
+const alchemyKey = process.env.REACT_APP_ALCHEMY_KEY;
+const { createAlchemyWeb3 } = require('@alch/alchemy-web3');
+const web3 = createAlchemyWeb3(alchemyKey);
+
+const contractABI = require('../contract-abi.json');
+const contractAddress = '0x4C4a07F737Bf57F6632B6CAB089B78f62385aCaE';
+
+export const mintNFT = async (url, name, description) => {
+	//error handling
+	if (url.trim() === '' || name.trim() === '' || description.trim() === '') {
+		return {
+			success: false,
+			status: '❗Please make sure all fields are completed before minting.',
+		};
+	}
+};
+
 const connectWallet = async () => {
 	if (window.ethereum) {
 		try {
